fd08924d77750e72f37bcbe98c6b8c756934e78c
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-01-11 17:42:27 -0500

    added joke app files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [joke, setJoke] = useState({
+    setup: "",
+    punchline: "",
+  });
+  const [showPunchline, setShowPunchline] = useState(false);
+  const [newJoke, setNewJoke] = useState(false);
+
+  // fetch the JSON inside React hook useEffect(callbackFunction, dependencyArray)
+  useEffect(() => {
+    fetch("https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes")
+      .then((res) => res.json())
+      .then((joke) =>
+        setJoke({
+          setup: joke.setup,
+          punchline: joke.punchline,
+        })
+      );
+  }, [newJoke]); // when newJoke state changes, run useEffect again
+
+  // button event handler to set state to show punchline
+  function showJokePunchline() {
+    setShowPunchline(true);
+  }
+
+  // button event handler to set state to get new joke
+  function getNewJoke() {
+    setShowPunchline(false);
+    setNewJoke((prevNewJoke) => !prevNewJoke);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="joke-container">
+      <h2 className="joke-header">Joke of the Day</h2>
+      <hr />
+      <h3 className="joke-setup">{joke.setup}</h3>
+      {showPunchline ? (
+        <div>
+          <p className="joke-punchline">{joke.punchline}</p>
+          <button className="joke-button" onClick={getNewJoke}>
+            New Joke
+          </button>
+        </div>
+      ) : (
+        <button className="joke-button" onClick={showJokePunchline}>
+          Show Punchline
+        </button>
+      )}
     </div>
   );
 }
-
-export default App;
